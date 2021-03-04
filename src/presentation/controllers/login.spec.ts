12929773b3513ec02e4c31eb075e3e38bcc158a3
ahsouza <annibalhsouza@gmail.com>
2021-03-03 21:59:53 -0300

    test: certifique LoginController retorna status code 400 se caso username nao for preenchido

---
+++ src/presentation/controllers/login.spec.ts
@@ -0,0 +1,17 @@
+import { LoginController } from './login'
+
+describe('Login Controller', ()=> {
+  test('Should return 400 case no username is provided', ()=>{
+    const sut = new LoginController()
+    
+    const httpRequest = {
+      body: {
+        username: 'seuusername',
+        password: 'suasenha',
+        password_confirmation: 'repetirsenha'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+  })
+})

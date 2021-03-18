ebd6ae18536941ae3572ff4166cc7229227cb095
ahsouza <annibalhsouza@gmail.com>
2021-03-17 21:52:42 -0300

    refactor: mover sut creation para método factory helper

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -1,14 +1,28 @@
 import { HttpResponse, HttpRequest } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { Controller } from '../protocols/controller'
+import { EmailValidator } from '../protocols/email-validator'
+import { InvalidParamError } from '../errors/invalid-param-error'
 
-export class LoginController {
+export class LoginController implements Controller {
+  private readonly emailValidator: EmailValidator
+
+  constructor (emailValidator: EmailValidator){
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
-    const fields = ['username', 'password', 'password_confirmation']
+    const fields = ['username', 'email', 'password', 'password_confirmation']
     for (const field of fields){
       if (!httpRequest.body[field]) {
         return badRequest(new MissingParamError(field))
       }
     }
+    const isValid = this.emailValidator.isValid(httpRequest.body.email)
+    if (!isValid) {
+      return badRequest(new InvalidParamError('email'))
+    }
+
   }
 }

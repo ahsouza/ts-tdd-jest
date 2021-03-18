809b76679362911370c747c2a2d2dde137738a34
ahsouza <annibalhsouza@gmail.com>
2021-03-17 21:55:01 -0300

    feat: certificar se LoginController retorna status code igual a 400 caso o email seja inválido

--- src/presentation/controllers/login.spec.ts
+++ src/presentation/controllers/login.spec.ts
@@ -1,15 +1,33 @@
 import { LoginController } from './login'
 import { MissingParamError } from '../errors/missing-param-error'
+import { InvalidParamError } from '../errors/invalid-param-error'
+import { EmailValidator } from '../protocols/email-validator'
 
-const makeLogin = (): LoginController => {
-  return new LoginController()
+interface SutTypes {
+  sut: LoginController
+  emailValidatorStub: EmailValidator
+}
+
+const makeLogin = (): SutTypes => {
+  class EmailValidatorStub implements EmailValidator {
+    isValid (email: string): boolean {
+      return true
+    }
+  }
+  const emailValidatorStub = new EmailValidatorStub()
+  const sut = new LoginController(emailValidatorStub)
+  return {
+    sut,
+    emailValidatorStub
+  }
 }
 
 describe('Login Controller', () => {
   test('Should return 400 case no username is provided', () => {
-    const sut = makeLogin()
+    const { sut } = makeLogin()
     const httpRequest = {
       body: {
+        email: 'seu@email.com.br',
         password: 'suasenha',
         password_confirmation: 'repetirsenha'
       }
@@ -17,5 +35,21 @@ describe('Login Controller', () => {
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new MissingParamError('username'))
+  })
+
+  test('Should return 400 case no email is provided', () => {
+    const { sut, emailValidatorStub } = makeLogin()
+    jest.spyOn(emailValidatorStub, 'isValid').mockReturnValueOnce(false)
+    const httpRequest = {
+      body: {
+        username: 'seu_username',
+        email: 'seu@email.com.br',
+        password: 'suasenha',
+        password_confirmation: 'repetirsenha'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new InvalidParamError('email'))
   })
 })

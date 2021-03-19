9aa373059df94045581cd4dbb3754e90c729ced1
ahsouza <annibalhsouza@gmail.com>
2021-03-19 20:00:32 -0300

    feat: ensure LoginController calls AddAccount with correct values

--- src/presentation/controllers/login.ts
+++ src/presentation/controllers/login.ts
@@ -4,29 +4,37 @@ import { badRequest, serverError } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from '../protocols/email-validator'
 import { InvalidParamError } from '../errors/invalid-param-error'
+import { AddAccount } from '../../domain/use-cases/add-account'
 
 export class LoginController implements Controller {
   private readonly emailValidator: EmailValidator
+  private readonly addAccount: AddAccount
 
-  constructor (emailValidator: EmailValidator){
+  constructor (emailValidator: EmailValidator, addAccount: AddAccount){
     this.emailValidator = emailValidator
+    this.addAccount = addAccount
   }
 
   handle (httpRequest: HttpRequest): HttpResponse {
     try{
       const fields = ['username', 'email', 'password', 'password_confirmation']
-      for (const field of fields){
+      for (const field of fields) {
         if (!httpRequest.body[field]) {
           return badRequest(new MissingParamError(field))
         }
       }
+      const { username, email, password } = httpRequest.body
       const isValid = this.emailValidator.isValid(httpRequest.body.email)
       if (!isValid) {
         return badRequest(new InvalidParamError('email'))
       }
+      this.addAccount.add({
+        username,
+        email,
+        password
+      })
     }catch (e){
       return serverError()
     }
-
   }
 }
